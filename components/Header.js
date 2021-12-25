fd3bd5237808173efdc0f5a5adba0744351d4d87
Darshan <darshanshah.ds2017@gmail.com>
2021-12-25 21:55:19 +0545

    Right of header - profile icon

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,5 @@
 import Image from 'next/image'
-import { SearchIcon } from '@heroicons/react/solid'
+import { SearchIcon, GlobeAltIcon, MenuIcon, UserCircleIcon } from '@heroicons/react/solid'
 
 function Header() {
     return (
@@ -24,8 +24,14 @@ function Header() {
             </div>
 
             {/* Right */}
-            <div>
+            <div className='flex space-x-4 items-center justify-end text-gray-500'>
+                <p className='hidden md:inline cursor-pointer'>Become a host</p>
+                <GlobeAltIcon className='h-6 cursor-pointer' />
                 
+                <div className='flex items-center border-2 p-2 rounded-full'>
+                    <MenuIcon className='h-6'/>
+                    <UserCircleIcon className='h-6 '/>
+                </div>
             </div>
         </header>
     )
