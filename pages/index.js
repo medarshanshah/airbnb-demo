15960213c441ecc97de951abba66087905241c0a
Darshan <darshanshah.ds2017@gmail.com>
2021-12-25 22:50:12 +0545

    Implementing server side rendering

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Header from '../components/Header'
 import Banner from '../components/Banner'
 
-export default function Home() {
+export default function Home({ exploreData }) {
   return (
     <div >
       <Head>
@@ -13,6 +13,31 @@ export default function Home() {
       <Header/>
       <Banner />
 
+    
+      <main className='max-w-7xl mx-auto px-8 sm:px-16'>
+        <section className='pt-6'>
+          <h2 className='text-4xl font-semibold pb-5'>Explore Nearby</h2>
+          
+          {/* Pulling data from server into exploreData prop*/}
+          {exploreData?.map((item) => (
+            <h1>{item.location}</h1>
+          ))}
+        </section>
+      </main>
+
     </div>
   )
+}
+
+{/* Pulling data from server */}
+export async function getStaticProps() {
+  const exploreData = await fetch('https://links.papareact.com/pyp').then(
+    (res) => res.json()
+  )
+
+  return {
+    props: {
+      exploreData,
+    }
+  }
 }
